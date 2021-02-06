0f6937ac5f8c3c110cbc4084dcfa4d7862959cd8
N-N <35467388+nsquares@users.noreply.github.com>
2021-02-05 22:15:15 -0500

    book hw2 done

--- CS141/bookHW2.cpp
+++ CS141/bookHW2.cpp
@@ -1,2 +1,21 @@
 // loops
 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int inputTemp, sumOfEvens;
+
+    cout<<"Give a list of even numbers and insert any odd number at the end: ";
+    cin>>inputTemp;
+
+    while ((inputTemp%2) == 0){
+        
+        sumOfEvens+=inputTemp;
+        cin>>inputTemp;
+    }
+    cout<<"Sum of the evens is "<<sumOfEvens<<endl;
+
+    return 0;
+}
